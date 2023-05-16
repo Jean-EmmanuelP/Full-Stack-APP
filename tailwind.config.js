5692cfa4cd58ec504cc047a35dbedfaf1d33350d
Jean-emmanuel PERRAMANT <jperrama@e2r1p5.clusters.42paris.fr>
2023-05-16 08:22:16 +0200

    first page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 const { colors } = require('tailwindcss/colors')
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     container: {
       center: true,
-      padding: "1.5rem",
+      padding: '1.5rem',
       screens: {
-        "2xl": "1360px",
+        '2xl': '1360px',
       },
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)", ...fontFamily.sans],
+        sans: ['var(--font-inter)', ...fontFamily.sans],
       },
       colors: {
         ...colors,
@@ -28,4 +32,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
-};
+}
