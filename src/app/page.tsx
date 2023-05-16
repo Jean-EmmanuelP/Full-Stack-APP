ab7fae13cb563605015f4e507033ff2fccb4daf6
JeanEmmPerramant <contact.perramant@gmail.com>
2023-05-16 02:56:37 +0200

    doing themetoggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Paragraph from './components/ui/Paragraph'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Paragraph from "../components/ui/Paragraph";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className='bg-red-500'><Paragraph size='sm' />some text</main>
+  return (
+    <main className="">
+      <Paragraph size="sm" />
+    </main>
+  );
 }
--- src/components/ui/Paragraph.tsx
+++ src/components/ui/Paragraph.tsx
@@ -1,6 +1,6 @@
 import { FC, forwardRef, HTMLAttributes } from 'react';
 import { cva, VariantProps } from 'class-variance-authority'
-import { cn } from '@/app/lib/utils';
+import { cn } from '@/lib/utils';
 
 
 
